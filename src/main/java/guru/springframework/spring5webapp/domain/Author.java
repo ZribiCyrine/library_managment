d77d7246f793fedaf04a2759c16a2721a2d3c03c
Cyrine <cyrinezribi23"@gmail.com>
2023-05-28 18:55:01 +0100

    fr_ls

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -23,9 +23,9 @@ public class Author {
     public Author() {
     }
 
-    public Author(String fName, String lName) {
-        this.firstName = fName;
-        this.lastName = lName;
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public Long getId() {
