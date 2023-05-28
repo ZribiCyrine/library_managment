4e8b4b82b36491be0bbb7bc955f40bc43388cb0f
Cyrine <cyrinezribi23"@gmail.com>
2023-05-28 19:13:35 +0100

    isb

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -25,9 +25,9 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn) {
+    public Book(String title, String is) {
         this.title = title;
-        this.isbn = isbn;
+        this.isbn = is;
     }
 
     public Long getId() {
