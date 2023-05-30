7ba5e261218484271c0f55fa640a5a6120ffc36c
Cyrine <cyrinezribi23"@gmail.com>
2023-05-30 13:05:12 +0100

    main

--- src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class Spring5webappApplicationTests {
 
